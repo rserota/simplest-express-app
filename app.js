9bd713036b37059bddc1ddb4ae5281d67f09371a
Raphael Serota <rserota@users.noreply.github.com>
2017-01-03 10:20:12 -0700

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var app = require('express')()
 
 app.get('/', function(req, res){
     console.log('we are writing logs!')
-    res.send("Welcome to the INTERWEBZ.")
+    res.send("It's all just a series of tubes.")
 })
 
 app.listen(80)
